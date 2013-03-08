bbda0139e35acb29a9d6daa533b4eb8787b89b9a
Anouar <anouar.kiassi@gmail.com>
2013-03-08 23:18:16 +0100

    init

---
+++ GestionFormationWeb/src/com/objis/demos/servlet/ListeFormationsObjis.java
@@ -0,0 +1,47 @@
+package com.objis.demos.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ListeFormationsObjis
+ */
+@WebServlet("/ListeFormationsObjis")
+public class ListeFormationsObjis extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListeFormationsObjis() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<HTML>");
+		out.println("<BODY>");
+		out.println("Liste des formations");
+		out.println("</BODY>");
+		out.println("</HTML>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
